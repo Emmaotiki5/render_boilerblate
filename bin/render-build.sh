55ab270a174e667a8925bee176577c00334909c7
Emmanuel Gbodo-Otiki <otikie507@gmail.com>
2023-12-22 04:21:54 +0100

    first commit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,5 @@ set -o errexit
 bundle install
 bundle exec rake assets:precompile --trace
 bundle exec rake assets:clean --trace
-bundle exec rails db:migrate
+bundle exec rails db:migrate --trace
+ 
