87ab02620422fee5f3a99d5a96cb586198518d5a
Emmanuel Gbodo-Otiki <otikie507@gmail.com>
2023-12-25 15:21:21 +0100

    Final Touches

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -76,6 +76,6 @@ class FriendsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def friend_params
-    params.require(:friend).permit(:first_name, :last_name, :email, :phone_no, :twitter)
+    params.require(:friend).permit(:first_name, :last_name, :email, :phone_no, :twitter, :image)
   end
 end
