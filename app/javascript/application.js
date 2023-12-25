4e3f96e1afad6cb216b029c38090c7ab8af49954
Emmanuel Gbodo-Otiki <otikie507@gmail.com>
2023-12-25 16:31:48 +0100

    Final Touches

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,22 @@ import * as bootstrap from "bootstrap"
 
 import "trix"
 import "@rails/actiontext"
+
+import "./add_jquery.js";
+
+function previewImage(input) {
+    const preview = document.getElementById('imagePreview');
+    preview.innerHTML = '';
+  
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const image = document.createElement('img');
+        image.src = e.target.result;
+        image.style.maxWidth = '300px'; // Adjust as needed
+        preview.appendChild(image);
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  
